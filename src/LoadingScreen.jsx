f87e2a7a5912cbc17ec71f69fa1f9c0ec4252f2a
Vaibhav Shukla <63441521+raxvab@users.noreply.github.com>
2022-02-02 16:15:54 +0530

    Chages the Loading page Animation

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import './CSSComponents/App.css';
-import CircularProgress from '@mui/material/CircularProgress';
+import "./CSSComponents/App.css";
+import CircularProgress from "@mui/material/CircularProgress";
 import { blue } from "@mui/material/colors";
 
 const Loader = () => {
-  return  (<div ><CircularProgress style={{marginTop:"50vh", marginLeft:"50vw"}} color="secondary" />
-  {/*<div style={{marginTop:"0vh", marginLeft:"45vw", color:"black",fontSize:"2vh" , fontWeight:"bold" }} >The PlannerApp is Loading </div>*/}
-  </div> )
+  return (
+ 
+      <div class="push-out">
+      
+        <div></div>
+        <div></div>
+      </div>
+   
+  );
 };
 
 export default Loader;
