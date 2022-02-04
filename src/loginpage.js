98a10b75fa806948753a5655296c7a7a38e42d15
navyagarwal <navya.agarwal.in@gmail.com>
2022-02-04 14:23:42 +0530

    changed-login-page-ui

--- src/loginpage.js
+++ src/loginpage.js
@@ -1,10 +1,12 @@
 import  Axios  from "axios";
 import React, { useState ,useContext } from "react";
+import { Link } from 'react-router-dom';
 import './CSSComponents/loginpage.css';
 import {  Route } from "react-router-dom";
 import {usercontext} from './Context/usercontext';
 
 import Calender from './calendar';
+import { color } from "@mui/system";
 
 const Login = () => {
 const {notloggedin,setloginstatus,userid,setuserid}=useContext(usercontext);    
@@ -20,6 +22,9 @@ const registeruser=()=>{
         useremail:useremail,
         password:password}).then(()=>{console.log("SUCCESS USER ADDED")})
 }
+
+
+
 const loginuser=()=>{
     Axios.post("https://planzap.herokuapp.com/userlogin",{
         useremail:lmail,
@@ -41,63 +46,45 @@ setmessage(response.data.message);
 }
 
   return <div className="loginpage">
-      <div style={{position:"Absolute",marginTop:"5vh",fontSize:"15vh",fontFamily:"serif",fontWeight:"bolder",color:"grey",textAlign:"center",width:"100vw"}} >PlanZap</div>
-      <div style={{position:"Absolute",marginTop:"21.7vh",fontSize:"3vh",fontFamily:"cursive",width:"100vw",textAlign:"center",color:"white"}} >One Place for managing Tasks, Personal Diary, Notes, movielist ! </div>
+      <div style={{position:"Absolute",marginTop:"5vh", fontSize:"15vh",fontFamily:"Pacificio",fontWeight:"bolder",color:"rgb(255, 183, 1)",textAlign:"right",width:"100vw"}} >PlanZap &nbsp; </div>
+
+      <div style={{position:"Absolute",marginTop:"21.7vh",fontSize:"3vh",fontFamily:"cursive",width:"100vw",textAlign:"right",color:"black"}} >One Place for your notes, goals, movies, personal diary and more ! &nbsp; &nbsp; &nbsp;</div>
+      <img style={{position: "fixed", bottom: 0, right: "5vw", width: "30vw",}} src="https://image.freepik.com/free-vector/flat-creativity-concept-illustration_52683-64279.jpg"></img>
 <div className="loginbox" >
-    <div className="signinbox">
-        <div className="heading" style={{fontSize:"5vh",fontWeight:"400"}}>
-            Login
+    <div className="loginform">
+        <center>
+        <i>"A good plan today is better than a perfect plan tomorrow"</i>
+        <br/>
+        <br/>
+        <div className="heading" style={{fontSize:"5vh",fontWeight:"400", color:"rgb(255, 183, 1)"}}>
+        <b>LOGIN</b>
             </div>
             <br/>
-            <br/>
-
-        <label style={{fontSize:"2.5vh",fontWeight:"300"}} for="emailid"><b>E-Mail ID</b></label>
+            
         <br/>
+        <form autocomplete="false">
         
-    <input style={{fontSize:"2.5vh"}} type="email" id="emailid" maxlength="40" name="emailid" className="holders" placeholder="Eg:abc@gmail.com" onChange={(event)=>{setlmail(event.target.value)}}/>
+    <input type="email" id="emailid" maxlength="40" name="emailid" className="holders" placeholder="Email" onChange={(event)=>{setlmail(event.target.value)}}/>
     <br/>
     <br/>
-    <label style={{fontSize:"2.5vh",fontWeight:"300"}} for="lpassword"><b>Password</b></label>
-        <br/>
-    <input style={{fontSize:"2.5vh"}} type="password" id="lpassword" maxlength="40" name="password" className="holders"  onChange={(event)=>{setpass(event.target.value)}}/>
+
+    <input type="password" id="lpassword" maxlength="10" name="password" className="holders" placeholder="Password" onChange={(event)=>{setpass(event.target.value)}}/>
     <br/>
+    <br/>    <br/>
+<button onClick={loginuser}><b>Sign In</b></button>
+<div>{message}</div>
 <br/>
 <br/>
-<button style={{backgroundColor:"teal",fontSize:"2vh", height:"auto",width:"auto",padding:"0.5vh",borderRadius:"10%",marginLeft:"10vw"}} onClick={loginuser}><b>Sign In</b></button>
-            <div>{message}</div>
+Visiting for the first time? &nbsp; &nbsp;<button>Register here!</button>
+</form>
+</center>
         </div>
-        <div className="signinbox"  style={{border:"none",borderRight:"none"}}>
-        <div className="heading"  style={{fontSize:"5vh",fontWeight:"400"}}>
-            Register
-            </div>
-            <br/>
-           
-
-        <form autocomplete="false"   >
-        <label style={{fontSize:"2.5vh",fontWeight:"300"}} for="emailid"><b>E-Mail ID</b></label>
-        <br/>
         
-    <input style={{fontSize:"2.5vh"}}  type="email" id="emailid" maxlength="40" name="hidden" autocomplete="false"  className="holders" placeholder="Eg:abc@gmail.com" onChange={(event)=>{setuseremail(event.target.value)}} />
-    <br/>
-    <br/>
-    <label style={{fontSize:"2.5vh",fontWeight:"300"}} for="password"><b>Password</b></label>
-        <br/>
-    <input style={{fontSize:"2.5vh"}}  type="password" id="password" maxlength="10" name="password"  autocomplete="new-password" className="holders"  />
-    <br/>
-<br/>
-<label style={{fontSize:"2.5vh",fontWeight:"300"}} for="Cpassword"><b>Confirm Password</b></label>
-        <br/>
-    <input type="password" id="Cpassword" maxlength="10" name="Cpassword" className="holders" onChange={(event)=>{setpassword(event.target.value)}} />
-    <br/>
-<br/>
-<br/>
-
-<button style={{backgroundColor:"teal",fontSize:"2vh", height:"auto",width:"auto",padding:"0.5vh",borderRadius:"10%",marginLeft:"10vw"}} onClick={registeruser}><b>Sign Up</b></button>
-            </form>
-            </div>
     </div>
 
   </div>;
 };
+        
+
 
 export default Login;
