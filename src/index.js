d0eb82301803e8d2fe4df5139f2ace539c6d85ef
Kushal <kushalghosh9899@gmail.com>
2022-02-22 16:47:46 +0530

    fix: Removed warnings from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
