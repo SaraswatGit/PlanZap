a95c0c5fda5396578c214c62afbbde4c51575b99
Saraswat Majumder <saraswatmajumder@gmail.com>
2022-02-28 12:45:22 +0530

    Project Restructuring Front-End

--- src/pgraphs.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './CSSComponents/App.css';
-
-const Pgraph = () => {
-  return (<div className="profpage"><h1>This is the Performance Page</h1></div>);
-};
-
-export  default Pgraph;
--- src/sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-  } from "react-pro-sidebar";
