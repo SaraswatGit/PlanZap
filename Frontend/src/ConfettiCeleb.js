2b6e321affd2b26a3ecbb4f2c7de1b14e1a3625e
Shikhar253 <saxenashikhar01@gmail.com>
2022-03-01 11:11:05 +0530

    Confetti celebration time reduced and transition made smooth . #97

--- Frontend/src/ConfettiCeleb.js
+++ Frontend/src/ConfettiCeleb.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Confetti from 'react-confetti'
+import React from "react";
+import Confetti from "react-confetti";
 
 export default function ConfettiCeleb() {
-    const confetti = {
-        width: '100vw',
-        height: '100vh',
-        position: 'fixed',
-    }
+  const confetti = {
+    width: "100vw",
+    height: "100vh",
+    position: "fixed",
+  };
 
-    return (
-        <div style={confetti}>
-            <Confetti
-                width={window.innerWidth}
-                height={window.innerHeight}
-              
-                opacity={0.9}
-                gravity={0.4}
-            />
-        </div>
-    )
+  return (
+    <div style={confetti}>
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        friction={0.99}
+        opacity={0.9}
+        gravity={0.1}
+        tweenDuration={2500}
+        recycle={false}
+      />
+    </div>
+  );
 }
