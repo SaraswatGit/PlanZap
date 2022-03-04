087a043214de1c93cc5571f0a5059a6cf4bffd6d
nitish saini <nitishsaini706@gmail.com>
2022-03-04 20:46:03 +0530

    dynamic height not working , hardcoded height of textarea to 5-6 rows

--- Frontend/src/idea.js
+++ Frontend/src/idea.js
@@ -21,13 +21,19 @@ function Idea({
   setnewidea,
   setnewdesc,
   settempid,
-}) {
+})
+ {
   const [isedit, setIsedit] = useState(false);
   const [isPopup, setPopup] = useState(false);
+ 
+  
+
+  
 
   // console.log(idea_list)
 
   return (
+
     <div>
       <div className="ideabox">
         <div className="ideaboxtop">
@@ -95,77 +101,79 @@ function Idea({
               <textarea
                 style={{
                   resize: "vertical",
-                  width: "80%"
+                  width: "50vw",
+                  height: "30vh"
                 }}
+
                 className="ideainput2"
                 defaultValue={tempideadesc}
                 onChange={(event) => {
                   setnewdesc(event.target.value);
                 }}
-                style={{height:"10rem"}}
+
               ></textarea>
               {" "}
-            </div>
-          ) : (
-            idea_list.idea_desc.split("\n").map((text) => (
-              <div>
-                {text}
-                <br />
-              </div>
-            ))
-          )}
         </div>
-
-        <Modal
-          isOpen={isPopup}
-          onRequestClose={() => {
-            setPopup(false);
-          }}
-          style={{
-            overlay: {
-              backgroundColor: "rgba(0, 0, 0, 0.75)",
-            },
-            content: {
-              width: "35vw",
-              height: "50vh",
-              margin: "auto",
-              padding: "1%",
-              borderRadius: "7px",
-              backgroundColor: "white",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "space-around",
-            },
-          }}
-          centered
-        >
-          <CancelIcon className="cross" />
-          <h2 className="delete-message">Do you want to delete the idea?</h2>
-          <div className="delete-btns">
-            <button
-              onClick={() => {
-                deletenote(idea_list.ideaid);
-                setPopup(false);
-              }}
-              className="popupBtn confirm-btn"
-              style={{ backgroundColor: "red" }}
-            >
-              confirm
-            </button>
-
-            <button
-              onClick={() => {
-                setPopup(false);
-              }}
-              className="popupBtn cancel-btn"
-            >
-              cancel
-            </button>
-          </div>
-        </Modal>
+        ) : (
+            idea_list.idea_desc.split("\n").map((text) => (
+        <div>
+          {text}
+          <br />
+        </div>
+        ))
+          )}
       </div>
+
+      <Modal
+        isOpen={isPopup}
+        onRequestClose={() => {
+          setPopup(false);
+        }}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+          content: {
+            width: "35vw",
+            height: "50vh",
+            margin: "auto",
+            padding: "1%",
+            borderRadius: "7px",
+            backgroundColor: "white",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-around",
+          },
+        }}
+        centered
+      >
+        <CancelIcon className="cross" />
+        <h2 className="delete-message">Do you want to delete the idea?</h2>
+        <div className="delete-btns">
+          <button
+            onClick={() => {
+              deletenote(idea_list.ideaid);
+              setPopup(false);
+            }}
+            className="popupBtn confirm-btn"
+            style={{ backgroundColor: "red" }}
+          >
+            confirm
+          </button>
+
+          <button
+            onClick={() => {
+              setPopup(false);
+            }}
+            className="popupBtn cancel-btn"
+          >
+            cancel
+          </button>
+        </div>
+      </Modal>
     </div>
+    </div >
   );
 }
 
