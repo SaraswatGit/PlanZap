602a37e977bf459bd4d9bd607b17a409c918105b
vakshit <akshitv18@gmail.com>
2022-03-13 22:48:01 +0530

    add backend for books

--- Backend/db.js
+++ Backend/db.js
@@ -4,10 +4,10 @@ const mysql = require("mysql");
 var db_config = {
     connectionLimit : 10,
 
- user:process.env.DATABASE_USERNAME,
+ user:"b5X2l394qY",
            host:"remotemysql.com",
-           password:process.env.DATABASE_PASSWORD,
-           database:process.env.DATABASE_USERNAME,
+           password:"YJgzFpNOr7",
+           database:"b5X2l394qY",
 
    };
 
