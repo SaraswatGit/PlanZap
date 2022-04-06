93b17ad594e80d798a7aca6f976c535f22f45c78
Kushal <kushalghosh9899@gmail.com>
2022-04-06 12:14:21 +0530

    fix: Add New quote button is working and User quote data is now visible
    *Removed seperated navbar component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import "./CSSComponents/App.css";
 import React, { useState, useEffect } from "react";
 import Login from "./loginpage";
 import Profile from "./profpage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,NavLink } from "react-router-dom";
 import Calender from "./calendar";
 import Loader from "./LoadingScreen";
 import Movies from "./movies";
@@ -15,7 +15,14 @@ import ConfettiCeleb from "./ConfettiCeleb";
 import MaintenanceImg from "./CSSComponents/maintenance.svg";
 import HowToUse from "./HowToUse";
 import Bookstoread from "./booksToRead";
-import Sidebar from "./Components/Sidebar";
+
+import EditIcon from "@mui/icons-material/Edit";
+import TaskIcon from "@mui/icons-material/Task";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 
 Modal.setAppElement("#root");
 
@@ -44,6 +51,7 @@ function App() {
   }, [isHowToUseModal]);
 
   function toggleModal() {
+    console.log("Clicked");
     setIsOpen(!isOpen);
   }
 
@@ -202,7 +210,114 @@ function App() {
             ) : (
               <div>
                 {" "}
-                <Sidebar />
+                
+                <div className="sidebar">
+      {/*      <NavLink to="/Profile" className="barelement" activeClassName="selected">
+                     <AccountCircleIcon />
+                     &nbsp; &nbsp; Profile
+              </NavLink>*/}
+      <div className="quotearea">
+        "{userquote}"<br />
+        <EditIcon className="editbutton" onClick={toggleModal} />
+      </div>
+      <NavLink
+        to="/TasksandProgress"
+        className={({ isActive }) =>
+          `link ${
+            isActive
+              ? "selected"
+              : // Couldn't do this before!
+                "barelement"
+          }`
+        }
+      >
+        <TaskIcon style={{ height: "2.8vh", marginRight: "1vw" }} />
+        Tasks and Progress
+      </NavLink>
+      {/*  <NavLink to="/Performancegraphs" className="barelement" activeClassName="selected">
+                     <ShowChartIcon/>      &nbsp; &nbsp;Performance Graphs
+    </NavLink>*/}
+      <NavLink
+        to="/movieslist"
+        className={({ isActive }) =>
+          `link ${
+            isActive
+              ? "selected"
+              : // Couldn't do this before!
+                "barelement"
+          }`
+        }
+      >
+        <FormatListBulletedIcon
+          style={{ height: "2.8vh", marginRight: "1vw" }}
+        />{" "}
+        Movies to Watch
+      </NavLink>
+      <NavLink
+        to="/diary"
+        className={({ isActive }) =>
+          `link ${
+            isActive
+              ? "selected"
+              : // Couldn't do this before!
+                "barelement"
+          }`
+        }
+      >
+        <MenuBookIcon style={{ height: "2.8vh", marginRight: "1vw" }} />{" "}
+        Personal Diary <br />
+      </NavLink>
+      <NavLink
+        to="/ideasnotes"
+        className={({ isActive }) =>
+          `link ${
+            isActive
+              ? "selected"
+              : // Couldn't do this before!
+                "barelement"
+          }`
+        }
+      >
+        <CollectionsBookmarkIcon
+          style={{ height: "2.8vh", marginRight: "1vw" }}
+        />{" "}
+        Ideas and Notes <br />
+      </NavLink>
+      <NavLink
+        to="/bookstoread"
+        className={({ isActive }) =>
+          `link ${
+            isActive
+              ? "selected"
+              : // Couldn't do this before!
+                "barelement"
+          }`
+        }
+      >
+        <LocalLibraryIcon style={{ height: "2.8vh", marginRight: "1vw" }} />{" "}
+        Books to Read <br />
+      </NavLink>
+      {/* <NavLink
+                    to="/howtouse"
+                    className={({ isActive }) =>
+                      `link ${
+                        isActive
+                          ? "selected"
+                          : // Couldn't do this before!
+                            "barelement"
+                      }`
+                    }
+                  >
+                    <ListAltIcon
+                      style={{ height: "2.8vh", marginRight: "1vw" }}
+                    />{" "}
+                    How To Use <br />
+                  </NavLink> */}
+      <div className="logout" onClick={logout} style={{ fontSize: "2.1vh" }}>
+        <LogoutIcon style={{ height: "2.8vh", marginRight: "1vw" }} /> Log Out{" "}
+      </div>      
+    </div>
+
                 <div>
                   <Modal
                     isOpen={isHowToUseModal}
