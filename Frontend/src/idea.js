cf4d93831d1214701f6fa08e353b18a280d66cc3
Danish <skdanish1999@gmail.com>
2022-05-30 15:59:54 +0530

    enhance the design of ideas and notes page

--- Frontend/src/idea.js
+++ Frontend/src/idea.js
@@ -25,150 +25,149 @@ function Idea({
   // console.log(idea_list)
 
   return (
-    <div>
-      <div className="ideabox">
-        <div className="ideaboxtop">
+    <div className="ideabox">
+      <div className="ideaboxtop">
+        {isedit ? (
+          <div>
+            <input
+              type="text"
+              defaultValue={tempidea}
+              onChange={(event) => {
+                setnewidea(event.target.value);
+              }}
+              className="ideainput"
+            />
+          </div>
+        ) : (
+          idea_list.idea_name
+        )}
+
+        <div
+          style={{
+            fontSize: "1.5vh",
+            display: "flex",
+          }}
+        >
           {isedit ? (
             <div>
-              <input
-                type="text"
-                defaultValue={tempidea}
-                onChange={(event) => {
-                  setnewidea(event.target.value);
+              <SaveIcon
+                onClick={() => {
+                  update();
+
+                  setIsedit(false);
                 }}
-                className="ideainput"
               />
             </div>
           ) : (
-            idea_list.idea_name
+            <EditIcon
+              className="hoverOnCursor"
+              onClick={() => {
+                settempidea(idea_list.idea_name);
+                settempdesc(idea_list.idea_desc);
+                setnewidea(idea_list.idea_name);
+                setnewdesc(idea_list.idea_desc);
+                settempid(idea_list.ideaid);
+                setIsedit(true);
+              }}
+            />
           )}
-
-          <div
-            style={{
-              fontSize: "1.5vh",
-              display: "flex",
-            }}
-          >
-            {isedit ? (
-              <div>
-                <SaveIcon
-                  onClick={() => {
-                    update();
-
-                    setIsedit(false);
-                  }}
-                />
-              </div>
-            ) : (
-              <EditIcon
+          &nbsp;{" "}
+          {isedit ? (
+            <div>
+              <CloseIcon
+                onClick={() => {
+                  setIsedit(false);
+                }}
+              />
+            </div>
+          ) : (
+            <div>
+              <CloseIcon
                 className="hoverOnCursor"
                 onClick={() => {
-                  settempidea(idea_list.idea_name);
-                  settempdesc(idea_list.idea_desc);
-                  setnewidea(idea_list.idea_name);
-                  setnewdesc(idea_list.idea_desc);
-                  settempid(idea_list.ideaid);
-                  setIsedit(true);
+                  setIsedit(false);
+                  setPopup(true);
                 }}
               />
-            )}
-            &nbsp;{" "}
-            {isedit ? (
-              <div>
-                <CloseIcon
-                  onClick={() => {
-                    setIsedit(false);
-                  }}
-                />
-              </div>
-            ) : (
-              <div>
-                <CloseIcon
-                  className="hoverOnCursor"
-                  onClick={() => {
-                    setIsedit(false);
-                    setPopup(true);
-                  }}
-                />
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="ideaboxbottom">
-          {isedit ? (
-            <div>
-              {" "}
-              <TextareaAutosize
-                style={{
-                  resize: "vertical",
-                }}
-                className="ideainput2"
-                defaultValue={tempideadesc}
-                onChange={(event) => {
-                  setnewdesc(event.target.value);
-                }}
-              />{" "}
             </div>
-          ) : (
-            idea_list.idea_desc.split("\n").map((text) => (
-              <div>
-                {text}
-                <br />
-              </div>
-            ))
           )}
         </div>
-
-        <Modal
-          isOpen={isPopup}
-          onRequestClose={() => {
-            setPopup(false);
-          }}
-          style={{
-            overlay: {
-              backgroundColor: "rgba(0, 0, 0, 0.75)",
-            },
-            content: {
-              width: "30vw",
-              height: "30vh",
-              margin: "auto",
-              padding: "1%",
-              borderRadius: "7px",
-              backgroundColor: "white",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "space-around",
-            },
-          }}
-          centered
-        >
-          <h2 className="delete-message">Do you want to delete the idea?</h2>
-          <div className="delete-btns">
-            <button
-              onClick={() => {
-                deletenote(idea_list.ideaid);
-                setPopup(false);
-              }}
-              className="popupBtn confirm-btn"
-              style={{ backgroundColor: "red" }}
-            >
-              confirm
-            </button>
-
-            <button
-              onClick={() => {
-                setPopup(false);
-              }}
-              className="popupBtn cancel-btn"
-            >
-              cancel
-            </button>
-          </div>
-        </Modal>
       </div>
+
+      <div className="ideaboxbottom">
+        {isedit ? (
+          <div>
+            {" "}
+            <TextareaAutosize
+              style={{
+                resize: "vertical",
+              }}
+              className="ideainput2"
+              defaultValue={tempideadesc}
+              onChange={(event) => {
+                setnewdesc(event.target.value);
+              }}
+            />{" "}
+          </div>
+        ) : (
+          idea_list.idea_desc.split("\n").map((text) => (
+            <div>
+              {text}
+              <br />
+            </div>
+          ))
+        )}
+      </div>
+
+      <Modal
+        isOpen={isPopup}
+        onRequestClose={() => {
+          setPopup(false);
+        }}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+          content: {
+            width: "30vw",
+            height: "30vh",
+            margin: "auto",
+            padding: "1%",
+            borderRadius: "7px",
+            backgroundColor: "white",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-around",
+          },
+        }}
+        centered
+      >
+        <h2 className="delete-message">Do you want to delete the idea?</h2>
+        <div className="delete-btns">
+          <button
+            onClick={() => {
+              deletenote(idea_list.ideaid);
+              setPopup(false);
+            }}
+            className="popupBtn confirm-btn"
+            style={{ backgroundColor: "red" }}
+          >
+            confirm
+          </button>
+
+          <button
+            onClick={() => {
+              setPopup(false);
+            }}
+            className="popupBtn cancel-btn"
+          >
+            cancel
+          </button>
+        </div>
+      </Modal>
     </div>
+  
   );
 }
 
